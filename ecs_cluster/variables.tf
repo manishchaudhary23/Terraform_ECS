b464ccf28401efbb83dbe87330d8038d15d7c67b
root <root@ip-10-0-3-137.ap-south-1.compute.internal>
2021-04-11 14:04:06 +0000

    ecscluster

---
+++ ecs_cluster/variables.tf
@@ -0,0 +1,67 @@
+variable "additional_user_data_script" {
+  description = "Additional user data script (default=\"\")"
+  default     = ""
+}
+
+variable "asg_max_size" {
+  description = "Maximum number EC2 instances"
+  default     = 2
+}
+
+variable "asg_min_size" {
+  description = "Minimum number of instances"
+  default     = 1
+}
+
+variable "asg_desired_size" {
+  description = "Desired number of instances"
+  default     = 1
+}
+
+variable "image_id" {
+  description = "AMI image_id for ECS instance"
+  default     = ""
+}
+
+variable "instance_keypair" {
+  description = "Instance keypair name"
+  default     = ""
+}
+
+variable "instance_log_group" {
+  description = "Instance log group in CloudWatch Logs"
+  default     = ""
+}
+
+variable "instance_root_volume_size" {
+  description = "Root volume size (default=50)"
+  default     = 50
+}
+
+variable "instance_type" {
+  description = "EC2 instance type (default=t2.micro)"
+  default     = "t2.micro"
+}
+
+variable "name" {
+  description = "Base name to use for resources in the module"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  default     = {}
+}
+
+variable "vpc_id" {
+  description = "VPC ID to create cluster in"
+  default     = "vpc-0434229e764d3f80a"
+}
+
+variable "vpc_subnets" {
+  description = "List of VPC subnets to put instances in"
+  default     = "subnet-0f961315483778441"
+}
+variable "vpc_subnets_elb" {
+  description = "List of VPC subnets to put instances in"
+  default     = ["subnet-0f961315483778441","subnet-0b5d070afb05c5174"]
+}
