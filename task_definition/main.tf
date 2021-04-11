dac892cb9f52c6e5970682aa27578cc36d43fbcb
root <root@ip-10-0-3-137.ap-south-1.compute.internal>
2021-04-11 14:07:41 +0000

    cretion of task

---
+++ task_definition/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_version = ">= 0.11"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key = "AKIAQIOE2V2KIJDD6UUZ"
+  secret_key = "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"
+}
+
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "service-first"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    },
+    {
+      name      = "second"
+      image     = "service-second"
+      cpu       = 10
+      memory    = 256
+      essential = true
+      portMappings = [
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+    }
+  ])
+
+  volume {
+    name      = "service-storage"
+    host_path = "/ecs/service-storage"
+  }
+
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:ecs.availability-zone in [ap-south-1]"
+  }
+}
