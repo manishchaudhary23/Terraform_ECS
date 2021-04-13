f17475bf0d63275a12400dc79d7b3a665d865d1a
root <root@ip-10-0-3-137.ap-south-1.compute.internal>
2021-04-13 12:18:05 +0000

    updated

---
+++ task_definition/variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  description = "Access key"
+  default     = "AKIAQIOE2V2KIJDD6UUZ"
+}
+
+variable "secret_key" {
+  description = "Secret key"
+  default     = "gYYk/fhU5IudLTzIE9kHkunbFf+5MyJecveYdPHO"
+}
